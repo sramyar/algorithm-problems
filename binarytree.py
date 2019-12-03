368c394fa86e9392bb4408e37cc70e89d6b5a586
Sepehr <sepehr.ramyar@gmail.com>
2019-12-02 17:01:52 -0800

    binary tree class complete

--- binarytree.py
+++ binarytree.py
@@ -1,3 +1,5 @@
+import queue
+
 class Node:
 
     def __init__(self, val):
@@ -10,6 +12,21 @@ class BinaryTree:
 
     def __init__(self):
         self.root = None
+        self.n = 0
+    
+    def add_child(self, parent, child):
+        if parent == None:
+            self.root = child
+        else:
+            if child.value > parent.value:
+                parent.right = child
+            elif child.value < parent.value:
+                parent.left = child
+            else:
+                return False
+            child.parent = parent
+        self.n += 1
+        return True
     
     def find_depth(self, u):
         d = 0
@@ -53,3 +70,109 @@ class BinaryTree:
                 next = u.parent
             prev = u
             u = next
+    
+    def bf_traverse(self):
+        u = self.root
+        q = queue.Queue()
+        q.put(u)
+        while not q.empty():
+            temp = q.get()
+            print(temp.value)
+            if temp.left != None:
+                q.put(temp.left)
+            if temp.right != None:
+                q.put(temp.right)
+            
+    def find_eq(self, val):
+        u = self.root
+        while u != None:
+            if u.value == val:
+                return u.value
+            elif val > u.value:
+                u = u.right
+            else:
+                u = u.left
+        return None
+
+    def find(self, val):
+        '''
+        Returns smallest value in the tree that greater than or equal to target val
+        '''
+        u = self.root
+        z = None
+        while u is not None:
+            if val < u.value:
+                z = u
+                u = u.left
+            elif val > u.value:
+                u = u.right
+            else:
+                z = u
+        if z is None:
+            return None
+        return z.value
+
+    def find_last(self, val):
+        u = self.root
+        prev = None
+        while u != None:
+            prev = u
+            if val > u.value:
+                u = u.right
+            elif val < u.value:
+                u = u.left
+            else:
+                return u
+        return prev
+    
+    def add(self, val):
+        u = self.find_last(val)
+        child = Node(val)
+        return self.add_child(u, child)
+
+    def splice(self, u: Node):
+        if u.left is not None:
+            child = u.left
+        else:
+            child = u.right
+
+        if u == self.root:
+            self.root = child
+            p = None
+        else:
+            p = u.parent   
+            if p.left == u:
+                p.left = child
+            else:
+                p.right = child
+
+        if child is not None:
+            child.parent = p
+        self.n -= 1
+        return True
+
+    def remove(self, u: Node):
+        if u.left is not None or u.right is not None:
+            self.splice(u)
+        # using the find(val, u) function above
+        #w = self.find(u.value, u.right)
+        else:
+            w = u.right
+            while w.left is not None:
+                w = w.left
+            u.value = w.value
+            self.splice(w)
+    
+
+tree = BinaryTree()
+tree.add(7)
+tree.add(8)
+tree.add(5)
+tree.add(7.5)
+tree.add(10)
+tree.bf_traverse()
+tree.add(100)
+tree.bf_traverse()
+
+    
+    
